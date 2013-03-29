dc4cae54fa517a0073361dfa234c31a6cfef8676
Tatsuya <tatsuya.sh.tiny@gmail.com>
2013-03-30 03:22:36 +0900

    Update

--- KiLibTools/KiLibGene_connector/main.cs
+++ KiLibTools/KiLibGene_connector/main.cs
@@ -37,9 +37,9 @@ namespace KiLibGene_connector
 					sw.WriteLine("#");
 					sw.WriteLine("# {0}", component_name);
 					sw.WriteLine("#");
-					sw.WriteLine("DEF {0} P 0 1 Y N 1 F N", component_name);
-					sw.WriteLine("F0 \"P\" -50 {0} 40 H V L CNN", 50 * (pin + 1));
-					sw.WriteLine("F1 \"{0}\" 50 -{1} 40 H V C CNN", component_name, 50 * (pin + 1));
+					sw.WriteLine("DEF {0} CN 0 1 Y N 1 F N", component_name);
+					sw.WriteLine("F0 \"CN\" -50 {0} 40 H V L CNN", 50 * pin + 30);
+					sw.WriteLine("F1 \"{0}\" 50 {1} 40 H V C CNN", component_name, -50 * pin -40);
 					sw.WriteLine("F2 \"~\" -50 {0} 60 H V C CNN", 50 * (pin - 1));
 					sw.WriteLine("F3 \"~\" -50 {0} 60 H V C CNN", 50 * (pin - 1));
 					sw.WriteLine("DRAW");
@@ -51,7 +51,35 @@ namespace KiLibGene_connector
 						sw.WriteLine("A 0 {0} 15 901 -901 0 1 1 F 0 {1} 0 {2}", pin_y, pin_y - 15, pin_y + 15);
 						sw.WriteLine("A 50 {0} 15 -899 899 0 1 1 F 50 {1} 50 {2}", pin_y, pin_y - 15, pin_y + 15);
 						sw.WriteLine("S 0 {0} 50 {1} 0 1 1 F", pin_y - 15, pin_y + 15);
-						sw.WriteLine("P 2 0 1 0  -50 {0}  50 {0} F", pin_y);
+						sw.WriteLine("P 2 0 1 0  -50 {0}  -15 {0} F", pin_y);
+					}
+					sw.WriteLine("ENDDRAW");
+					sw.WriteLine("ENDDEF");
+				}
+
+
+				for (int pin = 1; pin < MAX_PIN; pin++)
+				{
+					string component_name = "con-female-single-" + pin.ToString("D3");
+					sw.WriteLine("#");
+					sw.WriteLine("# {0}", component_name);
+					sw.WriteLine("#");
+					sw.WriteLine("DEF {0} CN 0 1 Y N 1 F N", component_name);
+					sw.WriteLine("F0 \"CN\" -50 {0} 40 H V L CNN", 50 * pin + 30);
+					sw.WriteLine("F1 \"{0}\" 50 {1} 40 H V C CNN", component_name, -50 * pin - 40);
+					sw.WriteLine("F2 \"~\" -50 {0} 60 H V C CNN", 50 * (pin - 1));
+					sw.WriteLine("F3 \"~\" -50 {0} 60 H V C CNN", 50 * (pin - 1));
+					sw.WriteLine("DRAW");
+					sw.WriteLine("S -50 {0} 150 -{0} 0 1 0 f", 50 * pin);		//外枠
+					for (int j = 0; j < pin; j++)
+					{
+						int pin_y = 100 * (pin - 1) / 2 - j * 100;
+						const int WIDTH = 18;
+						sw.WriteLine("X {0} {0} -200 {1} 150 R 50 50 1 1 P", j + 1, pin_y);		//ピン
+						sw.WriteLine("P 2 0 1 0  -50 {0}  -{1} {0} F", pin_y, WIDTH);
+						sw.WriteLine("A 0 {0} {3} 901 -901 0 1 0 N 0 {1} 0 {2}", pin_y, pin_y + WIDTH, pin_y - WIDTH, WIDTH);
+						sw.WriteLine("P 2 0 1 0  0 {0}  25 {0} F", pin_y + WIDTH);
+						sw.WriteLine("P 2 0 1 0  0 {0}  25 {0} F", pin_y - WIDTH);
 					}
 					sw.WriteLine("ENDDRAW");
 					sw.WriteLine("ENDDEF");
